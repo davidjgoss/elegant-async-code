67a211f5c5276a71e3c2005f346a968426a0e10d
David Goss <dgoss@whiteclarkegroup.com>
2016-02-20 10:13:48 +0000

    Refactored to a proper stateful component

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 (function() {
-    function loadMessages() {
+    function loadMessageList() {
         return fetch("data/messages.json").then(function(response) {
             return response.json();
         });
@@ -11,23 +11,39 @@
         }).length;
     }
 
-    function loadBody() {
+    function loadMessageBody(message) {
         return fetch("data/body.html").then(function(response) {
             return response.text();
+        }).then(function(body) {
+            return {
+                "from": message.from,
+                "subject": message.subject,
+                "body": body
+            };
         });
     }
 
-    var Message = React.createClass({
+    var Header = React.createClass({
+        render: function() {
+            return (
+                <header className="page-header">
+                    <h1>Inbox <small>{this.props.count} unread</small></h1>
+                </header>
+            );
+        }
+    });
+
+    var MessageItem = React.createClass({
         render: function() {
             var clickHandler = function(e) {
                 e.preventDefault();
-                showMessage(this.props);
+                this.props.openMessage(this.props.data);
             }.bind(this);
             return (
                 <a href="#" onClick={clickHandler} className="list-group-item">
-                    <h3 className="list-group-item-heading"><strong>{this.props.from}</strong></h3>
-                    <h4>{this.props.subject}</h4>
-                    <p>{this.props.snippet}</p>
+                    <h3 className="list-group-item-heading"><strong>{this.props.data.from}</strong></h3>
+                    <h4>{this.props.data.subject}</h4>
+                    <p>{this.props.data.snippet}</p>
                 </a>
             );
         }
@@ -35,9 +51,10 @@
 
     var MessageList = React.createClass({
         render: function() {
+            var openMessage = this.props.openMessage;
             var messageNodes = this.props.data.map(function(message) {
                 return (
-                    <Message key={message.id} id={message.id} from={message.from} subject={message.subject} snippet={message.snippet} />
+                    <MessageItem key={message.id} data={message} openMessage={openMessage} />
                 );
             });
             return (
@@ -48,7 +65,7 @@
         }
     });
 
-    var MessageBody = React.createClass({
+    var MessageView = React.createClass({
         rawMarkup: function() {
             return {__html: this.props.data.body};
         },
@@ -63,59 +80,61 @@
         }
     });
 
-    function populateList(messages) {
-        ReactDOM.render(
-            <MessageList data={messages} />,
-            document.querySelector("#list-container")
-        );
-        return messages;
-    }
-
-    function populateCounts(messages) {
-        // grab unread count from list of messages
-        var unread = countUnread(messages);
-
-        // populate count in title
-        ReactDOM.render(
-            <h1>Inbox <small>({unread} unread)</small></h1>,
-            document.querySelector("#header")
-        );
-
-        // title itself
-        document.title = "Inbox (" + unread + ")";
-
-        // maintain the chain
-        return messages;
-    }
-
-    function populateMessage(msg) {
-        ReactDOM.render(
-            <MessageBody data={msg} />,
-            document.querySelector("#view-container")
-        );
-    }
-
-    function showMessage(msg) {
-        return loadBody(msg.id).then(function(body) {
+    var Inbox = React.createClass({
+        updateCount: function(messages) {
+            var unread = countUnread(messages);
+            this.setState({unread: unread});
+            document.title = "Inbox (" + unread + ")";
+            return messages;
+        },
+        updateList: function(messages) {
+            this.setState({list: messages});
+            return messages;
+        },
+        openFirstMessage: function(messages) {
+            return this.openMessage(messages[0]);
+        },
+        openMessage: function(message) {
+            return loadMessageBody(message)
+                .then(this.updateView);
+        },
+        updateView: function(message) {
+            this.setState({view: message});
+        },
+        refreshMessages: function() {
+            return loadMessageList()
+                .then(this.updateCount)
+                .then(this.updateList)
+                .then(this.openFirstMessage);
+        },
+        componentDidMount: function() {
+            this.refreshMessages();
+        },
+        getInitialState: function() {
             return {
-                "from": msg.from,
-                "subject": msg.subject,
-                "body": body
+                unread: 0,
+                list: [],
+                view: {}
             };
-        }).then(populateMessage);
-    }
+        },
+        render: function() {
+            var openMessage = this.openMessage;
+            return (
+                <div className="inbox">
+                    <Header count={this.state.unread} />
+                    <div className="row">
+                        <div className="col-xs-12 col-sm-5 col-md-4">
+                            <MessageList data={this.state.list} openMessage={openMessage} />
+                        </div>
+                        <div className="col-xs-12 col-sm-7 col-md-8">
+                            <MessageView data={this.state.view} />
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+    });
 
-    function showFirstMessage(messages) {
-        return showMessage(messages[0]);
-    }
-
-    function refreshMessages() {
-        return loadMessages()
-            .then(populateCounts)
-            .then(populateList)
-            .then(showFirstMessage);
-    }
-
-    refreshMessages();
+    ReactDOM.render(<Inbox />, document.getElementById("app"));
 }());
 
